9c5c968e798c01f43debd4173c18c8d5ede5b153
AndrewN <andrewneilon@yahoo.com>
2024-09-30 01:29:53 +0200

    Updated API tests

---
+++ Tests/ApiTests.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.Playwright.NUnit;
+
+namespace Task2.Tests;
+
+[Parallelizable(ParallelScope.All)]
+[TestFixture]
+public class ApiTests : PageTest
+{
+    [Test]
+    public async Task RegisterAUser()
+    {
+        //Arrange
+
+        // All requests sent start with this API endpoint.
+        var baseURL = await Playwright.APIRequest.NewContextAsync(new() { BaseURL = "https://reqres.in/api/" });
+
+        // This is the request to GET user number 1
+        var request = await baseURL.PostAsync("register", new()
+        {
+            DataObject = new
+            {
+                email = "eve.holt@reqres.in",
+                password = "pistol"
+            }
+        });
+
+        var response = request.JsonAsync().Result.Value;
+        var response2 = request.JsonAsync().Result.GetValueOrDefault();
+
+        //Assert
+
+        //I decided to use FluentAssertions for these tests, to show C# versistilty
+        using (new AssertionScope())
+        {
+            request.Status.Should().Be(200);
+
+            response.Should().NotBeNull();
+            response.Should().Be("{\"id\":4,\"token\":\"QpwL5tke4Pnpja7X4\"}");
+        }
+    }
+
+    [Test]
+    public async Task GetaUser()
+    {
+        //Arrange
+
+        // All requests sent start with this API endpoint.
+        var baseURL = await Playwright.APIRequest.NewContextAsync(new() { BaseURL = "https://reqres.in/api/" });
+
+        // This is the request to GET user number 1
+        var request = await baseURL.GetAsync("users/1");
+
+        var response = request.JsonAsync().Result;
+
+        //Assert
+    }
+
+    [Test]
+    public async Task DeleteAUser()
+    {
+        //Arrange
+
+        // All requests sent start with this API endpoint.
+        var baseURL = await Playwright.APIRequest.NewContextAsync(new() { BaseURL = "https://reqres.in/api/" });
+
+        // This is the request to GET user number 1
+        var request = await baseURL.DeleteAsync("users/2");
+
+        var response = request.Status;
+        var response2 = request.StatusText;
+
+        //Assert
+    }
+}
--- Tests/UiTests.cs
+++ Tests/UiTests.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.Playwright.NUnit;
+﻿using FluentAssertions.Execution;
+using Microsoft.Playwright.NUnit;
 using NUnit.Framework.Internal;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace PlaywrightUiTests;
+namespace Task2.Tests;
 
 [Parallelizable(ParallelScope.Self)]
 [TestFixture]
@@ -28,6 +28,8 @@ public class UITests : PageTest
         Login();
 
         //Assert
+        using (new AssertionScope())
+        { 
         await Expect(Page).ToHaveURLAsync("https://www.saucedemo.com/inventory.html");
         await Expect(Page).ToHaveTitleAsync("Swag Labs");
 
@@ -36,6 +38,7 @@ public class UITests : PageTest
         await Expect(Page.Locator("[data-test=\"inventory-item-name\"]")).ToContainTextAsync(new string[] { "Sauce Labs Backpack", "Sauce Labs Bike Light", "Sauce Labs Bolt T-Shirt", "Sauce Labs Fleece Jacket", "Sauce Labs Onesie", "Test.allTheThings() T-Shirt (Red)" });
         await Expect(Page.Locator("[data-test=\"inventory-item-desc\"]")).ToContainTextAsync(new string[] { "carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.", "A red light isn't the desired state in testing but it sure helps when riding your bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included.", "Get your testing superhero on with the Sauce Labs bolt T-shirt. From American Apparel, 100% ringspun combed cotton, heather gray with red bolt.", "It's not every day that you come across a midweight quarter-zip fleece jacket capable of handling everything from a relaxing day outdoors to a busy day at the office.", "Rib snap infant onesie for the junior automation engineer in development. Reinforced 3-snap bottom closure, two-needle hemmed sleeved and bottom won't unravel.", "This classic Sauce Labs t-shirt is perfect to wear when cozying up to your keyboard to automate a few tests. Super-soft and comfy ringspun combed cotton." });
         await Expect(Page.Locator("[data-test=\"inventory-item-price\"]")).ToContainTextAsync(new string[] { "$29.99", "$9.99", "$15.99", "$49.99", "7.99", "$15.99" });
+        }
     }
 
     [Test]
@@ -67,27 +70,29 @@ public class UITests : PageTest
         await Page.GetByText("Continue").ClickAsync();
 
         //Assert
+        using (new AssertionScope())
+        {
+            await Expect(Page).ToHaveURLAsync("https://www.saucedemo.com/checkout-step-two.html");
+            await Expect(Page).ToHaveTitleAsync("Swag Labs");
 
-        await Expect(Page).ToHaveURLAsync("https://www.saucedemo.com/checkout-step-two.html");
-        await Expect(Page).ToHaveTitleAsync("Swag Labs");
+            await Expect(Page.Locator("[data-test=\"title\"]")).ToHaveTextAsync("Checkout: Overview");
+            await Expect(Page.Locator("[data-test=\"inventory-item\"]")).ToHaveCountAsync(2);
 
-        await Expect(Page.Locator("[data-test=\"title\"]")).ToHaveTextAsync("Checkout: Overview");
-        await Expect(Page.Locator("[data-test=\"inventory-item\"]")).ToHaveCountAsync(2);
+            await Expect(Page.Locator("[data-test=\"inventory-item-name\"]")).ToContainTextAsync(new string[] { "Sauce Labs Backpack", "Sauce Labs Bike Light" });
+            await Expect(Page.Locator("[data-test=\"inventory-item-desc\"]")).ToContainTextAsync(new string[] { "carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.", "A red light isn't the desired state in testing but it sure helps when riding your bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included." });
+            await Expect(Page.Locator("[data-test=\"inventory-item-price\"]")).ToContainTextAsync(new string[] { "$29.99", "$9.99" });
 
-        await Expect(Page.Locator("[data-test=\"inventory-item-name\"]")).ToContainTextAsync(new string[] { "Sauce Labs Backpack", "Sauce Labs Bike Light" });
-        await Expect(Page.Locator("[data-test=\"inventory-item-desc\"]")).ToContainTextAsync(new string[] { "carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.", "A red light isn't the desired state in testing but it sure helps when riding your bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included." });
-        await Expect(Page.Locator("[data-test=\"inventory-item-price\"]")).ToContainTextAsync(new string[] { "$29.99", "$9.99" });
+            await Expect(Page.Locator("[data-test=\"payment-info-label\"]")).ToHaveTextAsync("Payment Information:");
+            await Expect(Page.Locator("[data-test=\"payment-info-value\"]")).ToHaveTextAsync("SauceCard #31337");
 
-        await Expect(Page.Locator("[data-test=\"payment-info-label\"]")).ToHaveTextAsync("Payment Information:");
-        await Expect(Page.Locator("[data-test=\"payment-info-value\"]")).ToHaveTextAsync("SauceCard #31337");
+            await Expect(Page.Locator("[data-test=\"shipping-info-label\"]")).ToHaveTextAsync("Shipping Information:");
+            await Expect(Page.Locator("[data-test=\"shipping-info-value\"]")).ToHaveTextAsync("Free Pony Express Delivery!");
 
-        await Expect(Page.Locator("[data-test=\"shipping-info-label\"]")).ToHaveTextAsync("Shipping Information:");
-        await Expect(Page.Locator("[data-test=\"shipping-info-value\"]")).ToHaveTextAsync("Free Pony Express Delivery!");
-
-        await Expect(Page.Locator("[data-test=\"total-info-label\"]")).ToHaveTextAsync("Price Total");
-        await Expect(Page.Locator("[data-test=\"subtotal-label\"]")).ToHaveTextAsync("Item total: $39.98");
-        await Expect(Page.Locator("[data-test=\"tax-label\"]")).ToHaveTextAsync("Tax: $3.20");
-        await Expect(Page.Locator("[data-test=\"total-label\"]")).ToHaveTextAsync("Total: $43.18");
+            await Expect(Page.Locator("[data-test=\"total-info-label\"]")).ToHaveTextAsync("Price Total");
+            await Expect(Page.Locator("[data-test=\"subtotal-label\"]")).ToHaveTextAsync("Item total: $39.98");
+            await Expect(Page.Locator("[data-test=\"tax-label\"]")).ToHaveTextAsync("Tax: $3.20");
+            await Expect(Page.Locator("[data-test=\"total-label\"]")).ToHaveTextAsync("Total: $43.18");
+        }
     }
 
     [Test]
@@ -122,13 +127,15 @@ public class UITests : PageTest
         await Page.GetByText("Finish").ClickAsync();
 
         //Assert
+        using (new AssertionScope())
+        {
+            await Expect(Page).ToHaveURLAsync("https://www.saucedemo.com/checkout-complete.html");
+            await Expect(Page).ToHaveTitleAsync("Swag Labs");
 
-        await Expect(Page).ToHaveURLAsync("https://www.saucedemo.com/checkout-complete.html");
-        await Expect(Page).ToHaveTitleAsync("Swag Labs");
-
-        await Expect(Page.Locator("[data-test=\"title\"]")).ToHaveTextAsync("Checkout: Complete!");
-        await Expect(Page.Locator("[data-test=\"complete-header\"]")).ToHaveTextAsync("Thank you for your order!");
-        await Expect(Page.Locator("[data-test=\"complete-text\"]")).ToHaveTextAsync("Your order has been dispatched, and will arrive just as fast as the pony can get there!");
-        await Expect(Page.Locator("[data-test=\"back-to-products\"]")).ToHaveTextAsync("Back Home");
+            await Expect(Page.Locator("[data-test=\"title\"]")).ToHaveTextAsync("Checkout: Complete!");
+            await Expect(Page.Locator("[data-test=\"complete-header\"]")).ToHaveTextAsync("Thank you for your order!");
+            await Expect(Page.Locator("[data-test=\"complete-text\"]")).ToHaveTextAsync("Your order has been dispatched, and will arrive just as fast as the pony can get there!");
+            await Expect(Page.Locator("[data-test=\"back-to-products\"]")).ToHaveTextAsync("Back Home");
+        }
     }
 }
